163807c26973800e89a8cc22d987b38262d8f184
gracec11 <gracec11@uw.edu>
2020-01-17 15:48:53 -0800

    Add chapter 7 ex 2

--- chapter-07-exercises/exercise-2/exercise.R
+++ chapter-07-exercises/exercise-2/exercise.R
@@ -2,14 +2,14 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+v1 <- ("first_ten, 10":20)
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+v2 <- "next_ten seq(21,30)"
 
 # Create a vector `all_numbers` by combining the previous two vectors
-
+all_numbers <- "v1 + v2" 
 
 # Create a variable `eleventh` that contains the 11th element in `all_numbers`
 
