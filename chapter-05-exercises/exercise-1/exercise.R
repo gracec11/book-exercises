7519ae4e223db30b3acdb2ea17009bf9babe21ad
gracec11 <gracec11@uw.edu>
2020-01-19 00:00:58 -0800

    Add Chapter 5 Ex 1

--- chapter-05-exercises/exercise-1/exercise.R
+++ chapter-05-exercises/exercise-1/exercise.R
@@ -1,9 +1,27 @@
-earnings <- 150
-hours_worked <- 10
-pay_rate <- earnings / hours_worked
+# Exercise 1: practice with basic R syntax
 
-print ("earnings")
+# Create a variable `hometown` that stores the city in which you were born
+hometown <- ("Lakewood")
 
-pasted_result <- paste ("Hello", "World", "You rock")
-print (pasted_result)
- ?round
+# Assign your name to the variable `my_name`
+my_name <- ("Grace Chun")
+
+# Assign your height (in inches) to a variable `my_height`
+my_height <- ("62")
+
+# Create a variable `puppies` equal to the number of puppies you'd like to have
+puppies <- ("I want two puppies")
+
+# Create a variable `puppy_price`, which is how much you think a puppy costs
+puppy_price <- ("1000")
+
+# Create a variable `total_cost` that has the total cost of all of your puppies
+total_cost <- ("2000")
+
+# Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
+# than $1,000
+too_expensive <- ("TRUE")
+
+# Create a variable `max_puppies`, which is the number of puppies you can 
+# afford for $1,000
+max_puppies <- ("One puppy")
