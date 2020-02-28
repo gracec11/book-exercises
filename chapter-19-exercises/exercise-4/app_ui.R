6517ec3b927a538094f253ed218ca7156546841b
gracec11 <gracec11@uw.edu>
2020-02-27 16:11:29 -0800

    Add chapter 19 ex 4

--- chapter-19-exercises/exercise-4/app_ui.R
+++ chapter-19-exercises/exercise-4/app_ui.R
@@ -7,18 +7,21 @@ library("shiny")
 # Define a variable `intro_panel` for your first page. It should be a
 # `tabPanel()` with a title "Introduction" to represent the first tab.
 # This layout will contain the following elements:
+intro_panel <- tabPanel(
+  "Introduction",
   
   # A `titlePanel()` with the text "Income Inequality"
-  
+  titlePanel("Income Inequality"), 
 
   # A paragraph `p()` describing with the text: "The below diagram was created
   # by the New York Times to illustrate the increasing level of inequality in
   # the US."
-  
+  p("The below diagram was created by the New York Times to illustrate the
+    increasing level of inequality in the US."), 
 
   # An image `img()` to display. This content should have a `src` attribute of
   # "inequality.png" (which will refer to the file in the `www/` folder).
-  
+  img(src = "inequality.png"),
 
   # A paragraph containing a hyperlink `a()` to the source of the article at
   # https://www.nytimes.com/interactive/2017/08/07/opinion/leonhardt-income-inequality.html
